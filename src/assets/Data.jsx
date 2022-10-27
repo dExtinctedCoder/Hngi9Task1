c23ccd8c06268c9d4e52738e8c40dc7310c146ac
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-10-27 22:35:44 +0100

    Add assets(images and related data)

---
+++ src/assets/Data.jsx
@@ -0,0 +1,34 @@
+
+export const linkInfo = [
+  {
+    id: "twitter",
+    href: "https://twitter.com/dextinctedcoder",
+    subText: "Twitter Link",
+  },
+  {
+    id: 'btn__zuri',
+    href: "https://training.zuri.team/",
+    subText: "Zuri Team",
+  },
+  {
+    id: "books",
+    href: "http://books.zuri.team ",
+    subText: "Zuri Books",
+  },
+  {
+    id: "book__python",
+    href: `https://books.zuri.team/python-for-beginners?ref_id=<${"dEx"}>`,
+    subText: "Python Books",
+  },
+  {
+    id: "pitch",
+    href: "https://background.zuri.team",
+    subText: "Background Check for Coders",
+  },
+  {
+    id: "book__design",
+    href: "https://books.zuri.team/design-rules ",
+    subText: "Design Books",
+  },
+]
+// export default linkInfo
