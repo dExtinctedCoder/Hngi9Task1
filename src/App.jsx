d23c99605475586a2a2e609d889be947595aa064
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-03 16:42:13 +0100

    refactor: Configure app to use router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './index.css'
+import { Routes, Route } from 'react-router-dom'
 import Container from './components/Container'
+
 
 function App() {
 
   return (
     <div className="App">
-      <Container/>
+      <Routes>
+        <Route path="/Hngi9Task1/" element={<Container />}/>
+        {/* <Route path="/contact" element={}/> */}
+      </Routes>
     </div>
   )
 }
