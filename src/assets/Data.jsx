20344aa9f2f8a0d2eb5160b9244c50727d8af29f
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-03 16:59:09 +0100

    refactor: Modify render logic for button links

--- src/assets/Data.jsx
+++ src/assets/Data.jsx
@@ -2,33 +2,46 @@
 export const linkInfo = [
   {
     id: "twitter",
+    asLink: false,
     href: "https://twitter.com/dextinctedcoder",
     subText: "dExtinctedCoder",
   },
   {
     id: 'btn__zuri',
+    asLink: false,
     href: "https://training.zuri.team/",
     subText: "The Zuri Team",
   },
   {
     id: "books",
+    asLink: false,
     href: "http://books.zuri.team ",
     subText: "Zuri Books on design and coding",
   },
   {
     id: "book__python",
+    asLink: false,
     href: `https://books.zuri.team/python-for-beginners?ref_id=<${"dEx"}>`,
     subText: "Python Books",
   },
   {
     id: "pitch",
+    asLink: false,
     href: "https://background.zuri.team",
     subText: "Background Check for Coders",
   },
   {
     id: "book__design",
+    asLink: false,
     href: "https://books.zuri.team/design-rules ",
     subText: "Free Design Books",
+  },
+  {
+    id: "contact",
+    asLink: true,
+    to: "/contact",
+    href: "",
+    subText: "Contact",
   },
 ]
 // export default linkInfo
--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './links.css'
 import { linkInfo } from '../../assets/Data'
 
@@ -8,8 +9,13 @@ const Links = () => {
     <ul className="links__control">
       <li><a id='slack' style={{display: "none",}}>dEx</a></li>
       {
-        linkInfo.map(({id, href, subText}) => {
-          return <li key={id}><a href={href} target="_blank" id={id}>{subText}</a></li>
+        linkInfo.map(({id, href, subText, asLink, to}) => {
+
+          if (asLink) {
+            return <li key={id}><Link to={to ? to : ''} target="_blank" id={id}>{subText}</Link></li>
+          }
+
+          return <li key={id}><a href={href ? href : null} target="_blank" id={id}>{subText}</a></li>
         })
       }
     </ul>
