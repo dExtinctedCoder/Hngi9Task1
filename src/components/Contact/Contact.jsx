cfa0025706005a7a0a4d638be26638d5872cf5f2
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-03 17:38:46 +0100

    feat: base skeleton for contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,35 @@ const Contact = () => {
 
   return (
     <div>
-      Contact Page
+      <div className=" form-heading">
+        <h3>Contact Me</h3>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+      </div>
+      <form>
+        <section className=" form__proper">
+          <div className=" form__fields">
+            <label htmlFor="first-name"></label>
+            <input id="first_name" />
+          </div>
+          <div className=" form__fields">
+            <label htmlFor="last_name"></label>
+            <input  id="last_name" />
+          </div>
+          <div className=" form__fields">
+            <label htmlFor=" email"></label>
+            <input id="email" />
+          </div>
+          <div className=" form__fields">
+            <label htmlFor="message"></label>
+            <textarea id="message"></textarea>
+          </div>
+        </section>
+        <section className=" supporting__text">
+          <label htmlFor="checkbox">You agree to providing your data to {"dEx"} who may contact you.</label>
+          <input id="checkbox" type="checkbox" />
+        </section>
+        <button id="button__submit" type="submit">Send message</button>
+      </form>
     </div>
   )
 }
