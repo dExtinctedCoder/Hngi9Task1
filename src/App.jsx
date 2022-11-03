fd753852085c5344645a7cf60a42a9ba64949b7e
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-03 17:18:29 +0100

    refactor: Refactor codebase component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './index.css'
-import { Routes, Route } from 'react-router-dom'
 import Container from './components/Container'
-
 
 function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/Hngi9Task1/" element={<Container />}/>
-        {/* <Route path="/contact" element={}/> */}
-      </Routes>
+      <Container />
     </div>
   )
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Profile from './Profile/Profile'
 import Links from './Links/Links'
 import Socials from './Socials/Socials'
 import Footer from './Footer/Footer'
+import Contact from './Contact/Contact'
 import './base.css'
 
 const Container = () => {
   return (
     <>
       <div className="page__control">
-        <Profile/>
-        <Links />
-        <Socials />
+
+      <Routes>
+        <Route path="/Hngi9Task1/" element={[<Profile key={1}/>, <Links key={2}/>, <Socials key={3}/>]}>  
+          {/* <Profile/>
+          <Links />
+          <Socials /> */}
+        </Route>
+
+        <Route path="/Hngi9Task1/contact" element={<Contact />}/>
+      </Routes>
+
       </div>
       <Footer />
     </>
