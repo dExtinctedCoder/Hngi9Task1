8b4e89f7f33a805b058e23d295508b07c26d7f60
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-04 07:10:01 +0100

    feat: create customhook to control form fields

---
+++ src/components/hooks/useInput.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+function useInput(type) {
+  const [value, setValue] = useState('')
+  // const [password, setPassword] = useState('')
+
+  const stateControl = {
+    type: type,
+    value: value,
+    onChange: e => setValue(e.target.value),
+    // password: password,
+    // setPassword: setPassword,
+  }
+
+  return [stateControl]
+}
+
+export default useInput
